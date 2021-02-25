f67ceb3e348c128084f630984360a2b9d4bb6f17
andrerrcosta <andrerrcosta@gmail.com>
2021-02-25 17:46:52 -0300

    ...

--- client/sessions/src/views/ztest/ztest.js
+++ client/sessions/src/views/ztest/ztest.js
@@ -3,6 +3,11 @@ import { FloatBox } from "../../common/components/float-box/float-box";
 import { getElement } from "../../common/utils/dom.functions";
 import "./ztest.css";
 
+/**
+ * Well, at least in the applications that i do for myself
+ * i will not use reducer. I think reducer is useless.
+ */
+
 export default class ZTest extends React.Component {
 
     constructor() {
@@ -20,10 +25,15 @@ export default class ZTest extends React.Component {
     render() {
         return (
             <div className="ztest-container">
-                {/* <div style={{width: "200px", height: "50px", background: "blue"}} 
-                onMouseDown={() -> {}}>
+                Eu estou com raiva desse sharing states do react.
+                Eu vou inventar alguma coisa para isso, nem que eu tenha
+                que modificar o framework
+                <div style={{width: "200px", height: "50px", background: "blue"}} 
+                onMouseDown={() => {
 
-                </div> */}
+                }}>
+
+                </div>
                 
                 <FloatBox show={this.state.show} action={(action) => this.floatBoxChange(action)}>
                     Hello Box!
